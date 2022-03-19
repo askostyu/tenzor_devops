cd71c9859826299e26f88dd544fbfacc02c79a86
askostyuchenko <nghst@ngs.ru>
2022-03-19 09:37:45 -0400

    Ansible4_2 healthcheck_py done

---
+++ Ansible4_2/library/healthcheck_py.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from ansible.module_utils.basic import AnsibleModule
+import requests
+
+
+DOCUMENTATION = r'''
+---
+module: healthcheck
+author: Pupkin V.
+short_description: healthcheck of site
+description:
+  - healthcheck of site with or without TLS
+version_added: 1.0.0
+requirements:
+  - requests
+  - python >= 3.6
+options:
+  addr:
+    description:
+      - Address of site we want to check
+      - This is a required parameter
+    type: str
+  tls:
+    description:
+      - Whether site using certificates or not
+      - Default value is 'True'
+    type: bool
+'''
+
+EXAMPLES = r'''
+- name: Check availability of site
+  healthcheck:
+    addr: mysite.example
+  connection: local
+
+- name: Check availability of site without certs
+  healthcheck:
+    addr: mysite.example
+    tls: false
+  connection: local
+'''
+
+RETURN = r'''
+msg:
+  description: Errors if occured
+  returned: always
+  type: str
+  sample: ""
+site_status:
+  description: State status
+  returned: always
+  type: str
+  sample: Available
+rc:
+  description: Return code
+  returned: always
+  type: int
+  sample: 200
+'''
+
+def check_web_server(addr, tls):
+    site_status="Unavailable"
+    msg=""
+    rc = None
+
+    if tls:
+        url = f"https://{addr}"
+    else:
+        url = f"http://{addr}"
+    
+    try:
+        response = requests.get(url)
+    except Exception as ex:
+        msg = str(ex)
+    else:
+        rc = response.status_code
+        if rc == 200:
+            site_status = 'Available'
+    return msg, site_status, rc
+
+def main():
+    # Аргументы для модуля
+    arguments = dict(
+        addr=dict(required=True, type='str'),
+        tls=dict(type='bool', default="True")
+    )
+    # Создаем объект - модуль
+    module = AnsibleModule(
+        argument_spec=arguments,
+        supports_check_mode=False
+    )
+    # Получаем аргументы
+    addr = module.params["addr"]
+    tls = module.params["tls"]
+
+    msg, site_status, rc = check_web_server(addr, tls)
+
+
+    if msg:
+        module.fail_json(changed=False,
+                         failed=True,
+                         site_status=site_status,
+                         rc=rc,
+                         msg=msg)
+    else:
+        module.exit_json(changed=False,
+                         failed=False,
+                         result_str=site_status,
+                         rc=rc,
+                         msg=msg)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Ansible4_2/library/healthcheck_sh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# WANT_JSON
+
+addr=$(cat $1 | grep -Po '(?<="addr": ")(.*?)(?=")')
+tls=$(cat $1 | grep -Po '(?<="tls": )(.*?)(?=,)')
