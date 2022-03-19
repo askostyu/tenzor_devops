1303d434286aaf4299a8f1f9982d9fbd42fd2490
askostyuchenko <nghst@ngs.ru>
2022-03-19 06:36:41 -0400

    Ansible4_1 done

---
+++ Ansible4_1/filter_plugins/filter_base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import re
+
+from ansible.errors import (
+    AnsibleFilterTypeError
+)
+
+
+def to_mac(string):
+    '''
+        Converts string to mac-address
+    '''
+    is_mac_valid_pattern = r"^([0-9A-Fa-f]{2}){6}$"
+    if not re.search(is_mac_valid_pattern, string):
+        raise AnsibleFilterTypeError(f"Cant convert string {string} to mac-address")
+    mac_address = ":".join(re.findall(r"[0-9A-Fa-f]{2}", string))  
+    return mac_address
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'filter_function': to_mac
+        }
